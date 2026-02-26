8abead6c2af17f629651c85cc814d5a1a3508b2b
SaNdReX4 <sandro.biwkinashvili@gmail.com>
2026-02-27 03:41:08 +0400

    ended user-cards 320px

--- online-zoo/pages/landing/script.js
+++ online-zoo/pages/landing/script.js
@@ -1,3 +1,4 @@
+// burger menu
 const burger = document.getElementById('burger-btn');
 const closeBtn = document.getElementById('close-btn');
 const nav = document.getElementById('nav-menu');
@@ -10,3 +11,20 @@ closeBtn.addEventListener('click', () => {
     nav.classList.remove('active');
 });
 
+
+// cards
+const container = document.getElementById('cards-container');
+const dots = document.querySelectorAll('.dot');
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        
+        const activeDot = document.querySelector('.dot.active');
+        if (activeDot) activeDot.classList.remove('active');
+        dot.classList.add('active');
+
+        
+        const offset = index * 320; 
+        container.style.transform = `translateX(-${offset}px)`;
+    });
+});
